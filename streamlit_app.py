a348402530e5efadff9f35986e3582ae19b7c048
Iiris Esmaili <196097075+Iiris752@users.noreply.github.com>
2026-04-17 11:48:28 +0300

    projektin aloitus

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd 
+
+#Lähteet: http://docs.streamlit.io
+#jatka videosta kohdasta 14:17..
+
+# df = ...
+
+st.markdown(
+'''# Otsikko
+Toinen otsikko''')
+
+#st.dataframe(df)
